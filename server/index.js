cb7283c8beee0967bab87d38ff1f3f9b02ea58d2
Soujanya Chatti <67154494+SoujanyaChatti@users.noreply.github.com>
2025-04-21 17:04:27 +0530

    Update index.js
    Removing .env dependency for render deployment

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: '../.env' });
+//require('dotenv').config({ path: '../.env' });
 console.log('DATABASE_URL:', process.env.DATABASE_URL);
 const express = require('express');
 const { Pool } = require('pg');
